734f6eea7fd3c7cf291f8f5e0f71a42488215433
Juby210 <31005896+Juby210@users.noreply.github.com>
2021-10-23 13:03:34 +0200

    fixes

--- components/Settings.jsx
+++ components/Settings.jsx
@@ -1,36 +1,12 @@
 const { React } = require('powercord/webpack')
-const { RadioGroup, SwitchItem } = require('powercord/components/settings')
+const { RadioGroup } = require('powercord/components/settings')
 
-module.exports = class Settings extends React.PureComponent {
-    render() {
-        return <>
-            <SwitchItem
-                value={ this.props.getSetting('customBadge', true) }
-                onChange={ () => {
-                    this.props.toggleSetting('customBadge', true)
-                    if (this.props.getSetting('fixBadges')) this.toggleFixBadges()
-                }}
-                note='Custom badge can show max 99k+ mentions instead of 1k+ by default.'
-            >Use custom badge component</SwitchItem>
-            <SwitchItem
-                value={ this.props.getSetting('fixBadges') }
-                onChange={ () => this.toggleFixBadges() }
-                disabled={ !this.props.getSetting('customBadge', true) }
-            >Replace also other NumberBadges with custom badge</SwitchItem>
-            <RadioGroup
-                options={[
-                    { name: 'Home badge', value: 0 },
-                    { name: 'Home tooltip', value: 1 },
-                    { name: 'Text like online friends count', value: 2 }
-                ]}
-                value={ this.props.getSetting('display', 0) }
-                onChange={ e => this.props.updateSetting('display', e.value) }
-            >Display method</RadioGroup>
-        </>
-    }
-
-    toggleFixBadges() {
-        this.props.toggleSetting('fixBadges')
-        this.props.injectNumberBadge(this.props.getSetting('fixBadges'))
-    }
-}
+module.exports = ({ getSetting, updateSetting }) => <RadioGroup
+    options={[
+        { name: 'Home badge', value: 0 },
+        { name: 'Home tooltip', value: 1 },
+        { name: 'Text like online friends count', value: 2 }
+    ]}
+    value={ getSetting('display', 0) }
+    onChange={ e => updateSetting('display', e.value) }
+>Display method</RadioGroup>
--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const { Plugin } = require('powercord/entities')
-const { getModule, FluxDispatcher, React } = require('powercord/webpack')
+const { forceUpdateElement, findInReactTree } = require('powercord/util')
+const { getModule, Flux, FluxDispatcher, React } = require('powercord/webpack')
 const { inject, uninject } = require('powercord/injector')
 
-const Badge = require('./components/Badge')
 const Settings = require('./components/Settings')
-const UpdateableBadge = require('./components/UpdateableBadge')
-
-const n = getModule(['NumberBadge'], false)
-const getNestedProp = (e, t) => t.split('.').reduce((e, p) => e && e[p], e)
 
 module.exports = class MentionCount extends Plugin {
     async startPlugin() {
@@ -15,65 +11,38 @@ module.exports = class MentionCount extends Plugin {
         powercord.api.settings.registerSettings(this.entityID, {
             category: this.entityID,
             label: 'Mention Count',
-            render: p => React.createElement(Settings, { injectNumberBadge: this.injectNumberBadge, ...p })
+            render: Settings
         })
 
-        const { getTotalMentionCount: gm } = await getModule(['getGuildUnreadCount'])
+        const UnreadsStore = await getModule(['getTotalMentionCount'])
         const { listItem } = await getModule(['guildSeparator', 'listItem'])
-        const { DefaultHomeButton } = await getModule(['DefaultHomeButton'])
 
-        inject('mention-count', DefaultHomeButton.prototype, 'render', (_, res) => {
-            const d = this.settings.get('display', 0)
-
-            if (d == 2) {
-                res = [ res, React.createElement(UpdateableBadge, {
-                    Badge: ({ count }) => React.createElement('div', { className: listItem + ' mention-count' }, count),
-                    count: 0,
-                    _this: this,
-                    gm
-                })]
-                res.props = res[0].props
-                return res
-            }
-            const r = getNestedProp(res, 'props.children.props.children.props.children.props.children.1.props')
-            if (!r) return res
-            if (!d) {
-                const { props } = r.children
-                props.lowerBadge = React.createElement(UpdateableBadge, {
-                    Badge: this.settings.get('customBadge', true) ? Badge : n.NumberBadge,
-                    count: props.lowerBadge ? props.lowerBadge.props.count : 0,
-                    _this: this,
-                    gm
-                })
-            } else {
-                let count = gm()
-                this.last = count
-                r.text = `${count} Mention${count > 1 ? 's' : ''}`
-            }
-
+        const HomeButton = await getModule(m =>
+            typeof m.default === 'function' &&
+            (m.__powercordOriginal_default || m.default).toString().indexOf('showDMsOnly') !== -1
+        )
+        inject('mention-count', HomeButton, 'default', (_, res) => {
+            const { type } = res
+            res.type = Flux.connectStores([ UnreadsStore ], () => ({ mentionCount: UnreadsStore.getTotalMentionCount() }))(props => {
+                const d = this.settings.get('display', 0)
+                if (!d) return type({ ...props, badge: props.badge + props.mentionCount })
+                const ret = type(props)
+                if (d === 2) return [ ret, React.createElement('div', { className: listItem + ' mention-count' }, props.mentionCount) ] 
+                else {
+                    const tooltipProps = findInReactTree(ret, e => e && e.text)
+                    if (tooltipProps) tooltipProps.text = `${props.mentionCount} Mention${props.mentionCount > 1 ? 's' : ''}`
+                }
+                return ret
+            })
             return res
         })
+        HomeButton.default.toString = () => HomeButton.__powercordOriginal_default.toString()
 
-        FluxDispatcher.subscribe('MESSAGE_CREATE', this.updateBadge = () => {
-            const d = this.settings.get('display', 0)
-            if ((!d || d == 2) && this.badgeInstance && this.last != gm()) this.badgeInstance.forceUpdate()
-        })
-
-        this.injectNumberBadge(this.settings.get('fixBadges'))
+        forceUpdateElement(`.${getModule(['homeIcon', 'downloadProgress']).tutorialContainer}`)
     }
 
     async pluginWillUnload() {
         powercord.api.settings.unregisterSettings(this.entityID)
         uninject('mention-count')
-        this.injectNumberBadge(false)
-        if (this.updateBadge) FluxDispatcher.unsubscribe('MESSAGE_CREATE', this.updateBadge)
-    }
-
-    injectNumberBadge(bool) {
-        if (!bool) return uninject('mention-count-badge')
-        inject('mention-count-badge', n, 'NumberBadge', (args, res) => {
-            if (args[0] && args[0].count < 1000) return res
-            return React.createElement(Badge, args[0])
-        })
     }
 }
